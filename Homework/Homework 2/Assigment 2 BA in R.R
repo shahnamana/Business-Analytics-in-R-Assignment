af94959ad93e08346b76969d522fd1f724b2f7b5
Naman Shah <naman.as@somaiya.edu>
2022-09-29 23:12:41 -0500

    Updated code for Q1

--- Homework/Homework 2/Assigment 2 BA in R.R
+++ Homework/Homework 2/Assigment 2 BA in R.R
@@ -6,26 +6,21 @@ library(tidyverse)  # data manipulation
 library(cluster)    # clustering algorithms
 library(factoextra) # clustering algorithms & visualization
 
-
-
-
-
 # Q1
-dungaree <- read.csv("dungaree.csv")
+dungaree <- read.csv("C:/Users/shahn/Documents/GitHub/Business-Analytics-in-R-Assignment/Homework/Homework 2/dungaree.csv")
 # dungaree
 
 summary(dungaree)
 
-# a) As there would be need of all the columns to cluster the data, we will not drop any column.
-
+# a) As the column name Store id is of no use we would drop it.
+dungaree <- dungaree[,-1]
+summary(dungaree)
 
 # b) Examine the input variables: are there any unsual data values? Are there missing values that should be replaced
 # finding if there are any missing values
 sum(is.na(dungaree))
 
 # there are no missing values so we don't have to replace any values
-
-
 # Normalizing the data as the values in a few columns are larger which can cause abnormal behaviour in the scattering
 
 normalized_dungaree <- sapply(dungaree, scale)
@@ -39,7 +34,7 @@ km20 <- kmeans(normalized_dungaree, 20)
 
 # We will try to plot the kmeans cluster with k=20 with the following code
 plot_20<-fviz_cluster(km20, geom = "point", data = normalized_dungaree) + ggtitle("k = 20")
-
+plot_20
 # Determining the optimal clusters with the help of Elbow method
 # fviz_nbclust is the function used to do the elbow method for K means
 # here with the elbow method approach we can see that the optimal number of clusters required for the above normalized data would be 3. 
@@ -50,28 +45,29 @@ plot_20<-fviz_cluster(km20, geom = "point", data = normalized_dungaree) + ggtitl
 # Trying to plot the scatter plot for k=6
 km6 <- kmeans(normalized_dungaree,6)
 plot_6<-fviz_cluster(km6, geom = "point", data = normalized_dungaree) + ggtitle("k = 6")
-
-# k=3 plot for kmeans
-km3 <- kmeans(normalized_dungaree, 3)
-plot_3<-fviz_cluster(km3, geom = "point", data = normalized_dungaree) + ggtitle("k = 3")
-
-
+plot_6
 # printing all the plots together 
 
 # # plots to compare
-# p1 <- fviz_cluster(k2, geom = "point", data = df) + ggtitle("k = 2")
-# p2 <- fviz_cluster(k3, geom = "point",  data = df) + ggtitle("k = 3")
-# p3 <- fviz_cluster(k4, geom = "point",  data = df) + ggtitle("k = 4")
-# p4 <- fviz_cluster(k5, geom = "point",  data = df) + ggtitle("k = 5")
 # install.packages("gridExtra")
 library(gridExtra)
-grid.arrange(plot_20, plot_6, plot_3, nrow = 2)
-
-
+grid.arrange(plot_20, plot_6)
 # printing the centroids of the clusters with k=6
 km6$centers
 # plotting the centroids of the cluster
 # plot centroids
+# plot an empty scatter plot
+plot(c(0), xaxt = "n", ylab = "", type = "l", ylim = c(min(km6$centers), max(km6$centers)), xlim = c(0, 8))
+
+# label x-axes
+axis(1, at = c(1:8), labels = names(dungaree))
+
+# plot centroids
+for (i in c(1:6))
+    lines(km6$centers[i,], lty = i, lwd = 3, col = ifelse(i %in% c(1, 3, 5),
+
+
+
 plot(c(0), xaxt = 'n', ylab = "", type = "l", ylim = c(min(km6$centers), max(km6$centers)), xlim = c(0, 6))
 
 # label x-axes
--- Homework/Homework 2/Homework2_2.R
+++ Homework/Homework 2/Homework2_2.R
@@ -1,4 +1,7 @@
-# Q1
+# R code solution for Group 10
+
+# Importing various libraries
+# install.packages(c("tidyverse","factoextra", "cluster"))
 library(tidyverse) # data manipulation
 library(cluster) # clustering algorithms
 library(factoextra) # clustering algorithms & visualization
@@ -7,17 +10,16 @@ library(factoextra) # clustering algorithms & visualization
 dungaree <- read.csv("dungaree.csv")
 # dungaree
 summary(dungaree)
-# a) As there would be need of all the columns to cluster the data, we will not drop any column.
+# a) As the column name Store id is of no use we would drop it.
+dungaree <- dungaree[, -1]
+summary(dungaree)
 
 # b) Examine the input variables: are there any unsual data values? Are there missing values that should be replaced
 # finding if there are any missing values
 sum(is.na(dungaree))
 
 # there are no missing values so we don't have to replace any values
-
-
 # Normalizing the data as the values in a few columns are larger which can cause abnormal behaviour in the scattering
-
 normalized_dungaree <- sapply(dungaree, scale)
 # normalized_dungaree
 set.seed(42)
@@ -27,7 +29,7 @@ km20 <- kmeans(normalized_dungaree, 20)
 
 # We will try to plot the kmeans cluster with k=20 with the following code
 plot_20 <- fviz_cluster(km20, geom = "point", data = normalized_dungaree) + ggtitle("k = 20")
-
+plot_20
 # Determining the optimal clusters with the help of Elbow method
 # fviz_nbclust is the function used to do the elbow method for K means
 # here with the elbow method approach we can see that the optimal number of clusters required for the above normalized data would be 3.
@@ -38,82 +40,83 @@ plot_20 <- fviz_cluster(km20, geom = "point", data = normalized_dungaree) + ggti
 # Trying to plot the scatter plot for k=6
 km6 <- kmeans(normalized_dungaree, 6)
 plot_6 <- fviz_cluster(km6, geom = "point", data = normalized_dungaree) + ggtitle("k = 6")
-
-# k=3 plot for kmeans
-km3 <- kmeans(normalized_dungaree, 3)
-plot_3 <- fviz_cluster(km3, geom = "point", data = normalized_dungaree) + ggtitle("k = 3")
-
-
+plot_6
 # printing all the plots together
 
 # # plots to compare
 # install.packages("gridExtra")
 library(gridExtra)
-grid.arrange(plot_20, plot_6, plot_3, nrow = 2)
-
+grid.arrange(plot_20, plot_6)
 # printing the centroids of the clusters with k=6
 km6$centers
 # plotting the centroids of the cluster
-# plot centroids
-plot(c(0), xaxt = "n", ylab = "", type = "l", ylim = c(min(km6$centers), max(km6$centers)), xlim = c(0, 6))
+
+# plot an empty scatter plot
+plot(c(0),
+    xaxt = "n", ylab = "", type = "l",
+    ylim = c(min(km6$centers), max(km6$centers)), xlim = c(0, 5)
+)
 
 # label x-axes
-axis(1, at = c(1:6), labels = names(dungaree))
-for (i in c(1:6)) {
-    lines(km6$centers[i, ], lty = i, lwd = 3, col = ifelse(i %in% c(1, 3, 5), "black", "dark grey"))
+axis(1, at = c(1:5), labels = names(dungaree))
+
+# plot centroids
+for (i in c(1:5)) {
+    lines(km6$centers[i, ], lty = i, lwd = 3, col = ifelse(i %in% c(1, 3, 5),
+        "black", "dark grey"
+    ))
 }
+
 # name clusters
-text(x = 0.5, y = km6$centers[, 1], labels = paste("Cluster", c(1:6)))
+text(x = 0.5, y = km6$centers[, 1], labels = paste("Cluster", c(1:5)))
+
 # km6 centroids
 km6$centers
 dist(km6$centers)
 
 
 
-
-
-
 # Q2
 
-library(tidyverse)  # data manipulation
-library(cluster)    # clustering algorithms
+library(tidyverse) # data manipulation
+library(cluster) # clustering algorithms
 library(factoextra)
 
 setwd("E:/Khushi/UTD/BawithR/Datasets")
 pharma.df <- read.csv("Pharmaceuticals.csv")
 
 #--------------------2a------------------------
-row.names(pharma.df) <- pharma.df[,1]
+row.names(pharma.df) <- pharma.df[, 1]
 
-#Remove the symbol column
-pharma.df <- pharma.df[,-1]
+# Remove the symbol column
+pharma.df <- pharma.df[, -1]
 
-#Remove the Name column
-pharma.df <- pharma.df[,-1]
+# Remove the Name column
+pharma.df <- pharma.df[, -1]
 
-#Remove the Median_Recommendation column
-pharma.df <- pharma.df[,-10]
+# Remove the Median_Recommendation column
+pharma.df <- pharma.df[, -10]
 
-#Remove the Location column
-pharma.df <- pharma.df[,-10]
+# Remove the Location column
+pharma.df <- pharma.df[, -10]
 
-#Remove the Exchange column
-pharma.df <- pharma.df[,-10]
+# Remove the Exchange column
+pharma.df <- pharma.df[, -10]
 
 #--------------------2b------------------------
 
-#Normalize input variables
+# Normalize input variables
 pharma.df.norm <- sapply(pharma.df, scale)
 
-#Add row names: Symbols
+# Add row names: Symbols
 row.names(pharma.df.norm) <- row.names(pharma.df)
 
 #--------------------2c------------------------
 
-#Compute normalized distance based on all 9 variables
+# Compute normalized distance based on all 9 variables
 d.norm <- dist(pharma.df.norm, method = "euclidean")
 
-#In hclust(), set argument method = to "ward.D", "single", "complete", "average", "median", or 
+# In hclust(), set argument method = to "ward.D", "single", "complete", "average", "median", or
 "centroid"
 hc1 <- hclust(d.norm, method = "single")
 plot(hc1, hang = -1, ann = TRUE)
@@ -123,7 +126,7 @@ plot(hc1, hang = -1, ann = TRUE)
 memb <- cutree(hc1, k = 6)
 memb
 
-#Using hclust to show better grouping; dendexten
+# Using hclust to show better grouping; dendexten
 plot(hc1, cex = 1)
 rect.hclust(hc1, k = 6, border = 2:5)
 
@@ -137,13 +140,13 @@ plot(hc2, hang = -1, ann = TRUE)
 memb <- cutree(hc2, k = 6)
 memb
 
-#Using hclust to show better grouping; dendexten
+# Using hclust to show better grouping; dendexten
 plot(hc2, cex = 1)
 rect.hclust(hc2, k = 6, border = 2:5)
 
 #--------------------2g------------------------
 
-dend1 <- as.dendrogram (hc1)
-dend2 <- as.dendrogram (hc2)
+dend1 <- as.dendrogram(hc1)
+dend2 <- as.dendrogram(hc2)
 library(dendextend)
 tanglegram(dend1, dend2)
